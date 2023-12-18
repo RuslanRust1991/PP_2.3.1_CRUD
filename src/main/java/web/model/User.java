f39183ddd6670d1f449fc2330e9190a28a7f2f39
Ruslan <ruslan.rust@gmail.com>
2023-12-18 03:48:46 +0300

    2.3.1

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,74 @@
+package web.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getHobby() {
+
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+
+        this.hobby = hobby;
+    }
+
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(hobby, user.hobby);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, hobby);
+    }
+}
